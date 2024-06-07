c73940d5fe490bedbb9e4864a8da3e3a6270b3ff
copsman <copsman2011@gmail.com>
2024-06-07 23:14:44 +0000

    dynamic parent url

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ import Header from "@/components/Header";
 import { Button } from "@nextui-org/react";
 
 export default async function Index() {
+  const parentUrl = process.env.VERCEL_URL ? `${process.env.VERCEL_URL}` : "localhost";
   return (
     <>
-      <div>hello world</div>
       <iframe
-        src={`https://player.twitch.tv/?channel=compass_cs2&parent=localhost`}
+        src={`https://player.twitch.tv/?channel=compass_cs2&parent=${parentUrl}&muted=true`}
         height="500"
-        width="100%"
+        width="50%"
         allowFullScreen={true}>
       </iframe>
       <Button color="primary" variant="solid">
