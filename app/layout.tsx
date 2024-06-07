06e39a91d0385135e56c569a41af3d22a6d1b439
copsman <copsman2011@gmail.com>
2024-06-07 23:05:44 +0000

    added sample header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { Providers } from "./providers";
 import { NextUIProvider } from "@nextui-org/react";
+import NavbarHeader from "@/components/ui/layout/navbar/NavbarHeader";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body>
         <Providers>
           <main className="purple-dark min-h-screen flex flex-col items-center text-foreground bg-background">
+            <NavbarHeader />
             {children}
           </main>
         </Providers>
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,21 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import { Button } from "@nextui-org/react";
 
 export default async function Index() {
   return (
-    <div>hello world</div>
+    <>
+      <div>hello world</div>
+      <iframe
+        src={`https://player.twitch.tv/?channel=compass_cs2&parent=localhost`}
+        height="500"
+        width="100%"
+        allowFullScreen={true}>
+      </iframe>
+      <Button color="primary" variant="solid">
+        Solid
+      </Button>
+    </>
   )
 }
